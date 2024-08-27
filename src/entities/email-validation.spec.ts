aa3d388ea963732ff21054235b090fe1e3ed354c
rafaelsilva <todosomtodavoz@gmail.com>
2024-08-27 08:15:48 -0300

    test: validate local part large than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -11,8 +11,13 @@ describe('Email validation', () => {
     expect(Email.validate(email)).toBeFalsy()
   })
 
-  test('shpuld accept valid email', () => {
+  test('should accept valid email', () => {
     const email = 'any@mail.com'
     expect(Email.validate(email)).toBeTruthy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
   })
 })
