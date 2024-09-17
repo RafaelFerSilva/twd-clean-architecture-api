7b39915a24b512f15b6a29e72c15bdf5726492f2
rafaelsilva <todosomtodavoz@gmail.com>
2024-09-17 08:06:00 -0300

    refactor: make email constructor private

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -6,6 +6,6 @@ import { UserData } from './user-data'
 export class User {
   static create (userData: UserData): Either<InvalidEmailError, User> {
     const emailOrError = Email.create(userData.email)
-    if (emailOrError.isLeft) return left(new InvalidEmailError())
+    if (emailOrError.isLeft()) return left(new InvalidEmailError())
   }
 }
