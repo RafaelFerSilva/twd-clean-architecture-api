9209f206716e23754781e0b3426c16c54efee712
rafaelsilva <todosomtodavoz@gmail.com>
2024-09-26 07:58:50 -0300

    chore: add index.ts for each file

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -18,8 +18,8 @@ export class User {
     const nameOrError = Name.create(userData.name)
     const emailOrError = Email.create(userData.email)
 
-    if (nameOrError.isLeft()) return left(new InvalidNameError(userData.name))
-    if (emailOrError.isLeft()) return left(new InvalidEmailError(userData.email))
+    if (nameOrError.isLeft()) return left(nameOrError.value)
+    if (emailOrError.isLeft()) return left(emailOrError.value)
 
     const name: Name = nameOrError.value as Name
     const email: Email = emailOrError.value as Email
