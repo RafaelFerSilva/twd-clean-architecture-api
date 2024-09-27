d7a015c0c86611767951fb114521c53464a795e1
rafaelsilva <todosomtodavoz@gmail.com>
2024-09-27 08:41:26 -0300

    test: should return status code 201 when request contains valid user data

---
+++ test/web-controllers/sign-up-controller.spec.ts
@@ -0,0 +1,25 @@
+import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { HttpRequest } from '@/web-controllers/ports'
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { RegisterUserController } from '@/web-controllers/register-user-controller'
+import { HttpResponse } from '@/web-controllers/ports/http-response'
+
+describe('Register user web controller', () => {
+  test('should return status code 201 when request contains valid user data', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any name',
+        email: 'any@email.com'
+      }
+    }
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const controller: RegisterUserController = new RegisterUserController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual(request.body)
+  })
+})
