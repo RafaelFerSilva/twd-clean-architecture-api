a4c52c3793a603fb7f75c7f73f01ffb678a4f9ed
rafaelsilva <todosomtodavoz@gmail.com>
2024-10-17 07:28:18 -0300

    feat: register and send email use case

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,12 +1,17 @@
 import { RegisterAndSendEmailController } from '@/web-controllers'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { MongodbUserRepository } from '@/external/repositories/mongodb'
-// import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
+import { SendEmail } from '@/usecases/send-email'
+import { NodeMailerEmailService } from '@/external/mail-services'
+import { getEmailOptions } from '../config/email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email'
 
 export const makeRegisterAndSendEmailController = (): RegisterAndSendEmailController => {
-  // const inMemoryUserRepository = new InMemoryUserRepository([])
   const mondoDbUserRepository = new MongodbUserRepository()
-  const registerUserOnMailingList = new RegisterUserOnMailingList(mondoDbUserRepository)
-  const registerAndSendEmailController = new RegisterAndSendEmailController(registerUserOnMailingList)
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mondoDbUserRepository)
+  const emailService = new NodeMailerEmailService()
+  const sendEmailUseCase = new SendEmail(getEmailOptions(), emailService)
+  const registerAndSendEmailUseCase = new RegisterAndSendEmail(registerUserOnMailingListUseCase, sendEmailUseCase)
+  const registerAndSendEmailController = new RegisterAndSendEmailController(registerAndSendEmailUseCase)
   return registerAndSendEmailController
 }
